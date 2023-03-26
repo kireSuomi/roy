a0021c6fac9608b74ab2a664113a2a916593dad2
unknown <kireSuomi@gmail.com>
2023-03-26 14:14:48 +0200

    Add head component

---
+++ roy/components/Head.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function HeadComp() {
+  return (
+    <Head>
+      <title>ROY - Code test</title>
+      <meta name="description" content="Code test for roy" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/logo.svg" />
+    </Head>
+  );
+}
